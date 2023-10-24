a6467fb28defbd73ec152eff89472bf21b9ac7eb
Noverry Ambo <noverryambo@gmail.com>
2023-10-24 22:59:26 +0700

    first deploy

---
+++ src/test/java/is/product/sampleproduct/SampleProductApplicationTests.java
@@ -0,0 +1,13 @@
+package is.product.sampleproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
